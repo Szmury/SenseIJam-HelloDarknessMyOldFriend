6374165cf031b8c1c36b3fb0d2cfc2263fd59a02
Mateusz Kacała <293070@uwr.edu.pl>
2018-12-08 10:52:57 +0100

    Fire particles

--- HelloDarknessMyOldFriend/Assets/Scripts/GremlinController.cs
+++ HelloDarknessMyOldFriend/Assets/Scripts/GremlinController.cs
@@ -1,23 +1,33 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GremlinController : MonoBehaviour {
 
     public Action<GameObject> ToPool;
+    public GameObject particle;
+    private float delay=3f;
 
-	public void Burn()
+
+    public void Burn()
     {
         Debug.Log("I'm burning!");
-        Death();
-        // Start animations here
+        ParticleSystem part = particle.GetComponent<ParticleSystem>();
+        particle.SetActive(true);
+        part.Play();
+        delay = part.main.duration;
+        StartCoroutine(Death());
     }
-
-    public void Death()
+    
+    IEnumerator Death()
     {
+        yield return new WaitForSeconds(delay);
+        particle.SetActive(false);
         ToPool(gameObject);
         Score.score.AddScore();
         gameObject.SetActive(false);
     }
+    
 
 }
