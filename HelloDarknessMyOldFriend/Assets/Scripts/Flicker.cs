426c7ba48552d9c339e635d462901faff22bff07
Mateusz Kacała <293070@uwr.edu.pl>
2018-12-09 10:52:00 +0100

    Flicker

---
+++ HelloDarknessMyOldFriend/Assets/Scripts/Flicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour {
+
+    public float delay=1f;
+
+    public GameObject lightL;
+    public GameObject lightR;
+    private float nextTime;
+
+    // Use this for initialization
+    void Start () {
+        nextTime = Time.time + delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time >= nextTime)
+        {
+            nextTime = Time.time + delay;
+            if(lightL.activeInHierarchy)
+            {
+                lightL.SetActive(false);
+                lightR.SetActive(false);
+            }
+            else
+            {
+                lightL.SetActive(true);
+                lightR.SetActive(true);
+            }
+        }
+	}
+}
